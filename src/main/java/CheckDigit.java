26e82da904091a71b4b85d38015300c9875e83da
stli12 <143028001+stli12@users.noreply.github.com>
2024-04-29 12:56:05 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -4,9 +4,8 @@ public class CheckDigit
    public static int getCheck(int num) 
    {  
      int sum = 0;
-     int temp = num;
      for(int i = 1;  i <= getNumberOfDigits(num); i++){
-  sum += (8-1) * getDigits( temp , i);
+  sum += (8-1) * getDigits(num , i);
      }
      return sum % 10;
    }
